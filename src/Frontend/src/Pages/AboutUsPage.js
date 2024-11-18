3b565d36585baf03d38afd198061fa356fcfcadc
Gustavo <164703668+GuKstro@users.noreply.github.com>
2024-11-18 19:14:17 -0300

    Add files via upload

---
+++ src/Frontend/src/Pages/AboutUsPage.js
@@ -0,0 +1,17 @@
+import Header from "../components/Header/Header";
+import Recomendacoes from "../components/BarraDeRecomendacao";
+import AboutUsContent from "../components/AboutUsContent";
+import Footer from '../components/Footer'
+
+const AboutUsPage = () =>{
+    return(
+        <>
+            <Header/>
+            <Recomendacoes/>
+            <AboutUsContent/>
+            <Footer/>
+        </>
+    )
+}
+
+export default AboutUsPage
