5496b66a34bf29a2820d3cd06aec23f86c170f48
marcella022 <marcellasantanaprofissional@gmail.com>
2024-11-18 15:38:18 -0300

    Create SupportPage.js

---
+++ src/Frontend/src/Pages/SupportPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import Recomendacoes from "../components/BarraDeRecomendacao";
+import SupportContent from "../components/SupportContent";
+import Footer from "../components/Footer";
+
+const SupportPage = () =>{
+    return(
+        <>
+        <Header/>
+        <Recomendacoes/>
+        <SupportContent/>
+        <Footer/>
+        </>
+    )
+}
+
+export default SupportPage
